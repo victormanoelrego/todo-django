710dde7b64d2eb1b358d17ab78cdb9ae28ebc19c
SarahIzabely <sarah.freitas@escolar.ifrn.edu.br>
2025-08-10 23:34:57 -0300

    Implementa criação, edição e exclusão de tarefas (views/templates)

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,5 +5,8 @@ app_name = 'tasks'
 
 urlpatterns = [
     path('', views.TaskListView.as_view(), name='list'),
+    path('new/', views.TaskCreateView.as_view(), name='create'),
     path('<int:pk>/', views.TaskDetailView.as_view(), name='detail'),
+    path('<int:pk>/edit/', views.TaskUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.TaskDeleteView.as_view(), name='delete'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Task
 
 class TaskListView(ListView):
@@ -10,3 +11,20 @@ class TaskListView(ListView):
 class TaskDetailView(DetailView):
     model = Task
     template_name = 'tasks/task_detail.html'
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = ['title', 'description', 'due_date', 'completed']
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('tasks:list')
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    fields = ['title', 'description', 'due_date', 'completed']
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('tasks:list')
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'tasks/task_confirm_delete.html'
+    success_url = reverse_lazy('tasks:list')
